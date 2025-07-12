06f36cb81454bc73e24b12b76c1c0e61df7fd350
SERDUN <dmitriiserdun@gmail.com>
2025-07-12 20:06:21 +0300

    feat: init logging

---
+++ main/blink/blink_task.c
@@ -0,0 +1,8 @@
+#include "esp_log.h"
+#include "blink_task.h"
+
+static const char *TAG = "BLINK";
+
+void blink_task_start(void) {
+    ESP_LOGI(TAG, "Blink task started");
+}
--- main/main.c
+++ main/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "esp_log.h"
+#include "blink/blink_task.h"
+
+static const char *TAG = "APP_CONSOLE";
 
 void app_main(void)
 {
-
+    ESP_LOGI(TAG, "Initializing app...");
+    blink_task_start();
 }
